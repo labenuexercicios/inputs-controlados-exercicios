b7f689c6b3c97108924c401a6437e7b24ab3a247
Luiz Fernando Barbosa <71241157+luizfbarbosa12@users.noreply.github.com>
2022-10-18 10:56:17 -0300

    remove input types

--- src/Components/NameForm/NameForm.js
+++ src/Components/NameForm/NameForm.js
@@ -1,26 +1,26 @@
 import React from 'react'
 import { Form, Input} from '../MainPage/styles'
 
-const NameForm = () => {
+const NameForm = (props) => {
   return (
     <Form>
         <label>
           Nome:
-          <Input type={'text'} placeholder="Nome" value={name} onChange={onChangeName}/>
+          <Input  placeholder="Nome" value={props.name} onChange={props.onChangeName}/>
         </label>
         <label>
           Idade:
-          <Input type={'number'} placeholder="Idade" value={age} onChange={onChangeAge}/>
+          <Input  placeholder="Idade" value={props.age} onChange={props.onChangeAge}/>
         </label>
         <label>
           E-mail:
-          <Input type={'email'} placeholder="usuario@usuario.com" value={email} onChange={onChangeEmail}/>
+          <Input  placeholder="usuario@usuario.com" value={props.email} onChange={props.onChangeEmail}/>
         </label>
         <label>
           Confirmação de e-mail:
-          <Input type={'email'} placeholder="usuario@usuario.com" value={email} onChange={onChangeEmail}/>
+          <Input  placeholder="usuario@usuario.com" value={props.email} onChange={props.onChangeEmail}/>
         </label>
-      <button onClick={sendData}>Enviar dados</button>
+      <button onClick={props.sendData}>Enviar dados</button>
       </Form>
   )
 }
