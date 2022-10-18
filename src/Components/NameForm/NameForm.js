128b002221b43fa64233b5ccae41e0cb14859d5a
Luiz Fernando Barbosa <71241157+luizfbarbosa12@users.noreply.github.com>
2022-10-18 10:59:22 -0300

    remove props

--- src/Components/NameForm/NameForm.js
+++ src/Components/NameForm/NameForm.js
@@ -6,21 +6,21 @@ const NameForm = (props) => {
     <Form>
         <label>
           Nome:
-          <Input  placeholder="Nome" value={props.name} onChange={props.onChangeName}/>
+          <Input  placeholder="Nome" value={name} onChange={onChangeName}/>
         </label>
         <label>
           Idade:
-          <Input  placeholder="Idade" value={props.age} onChange={props.onChangeAge}/>
+          <Input  placeholder="Idade" value={age} onChange={onChangeAge}/>
         </label>
         <label>
           E-mail:
-          <Input  placeholder="usuario@usuario.com" value={props.email} onChange={props.onChangeEmail}/>
+          <Input  placeholder="usuario@usuario.com" value={email} onChange={onChangeEmail}/>
         </label>
         <label>
           Confirmação de e-mail:
-          <Input  placeholder="usuario@usuario.com" value={props.email} onChange={props.onChangeEmail}/>
+          <Input  placeholder="usuario@usuario.com" value={email} onChange={onChangeEmail}/>
         </label>
-      <button onClick={props.sendData}>Enviar dados</button>
+      <button onClick={sendData}>Enviar dados</button>
       </Form>
   )
 }
